d48c61b7b8eb04ecb275f87b7e00c46d86824205
Iaroslav Uliantcev <doudou239@gmail.com>
2025-10-02 20:39:12 +0200

    Add WG logo to header.

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,7 +1,17 @@
+import wgLogo from '@/assets/images/wg_logo.svg';
+import { SvgIcon } from '@/shared/utils/SvgIcon';
 import { h } from '@/shared/utils/h';
 
 import styles from './Header.module.scss';
 
 export function Header() {
-  return h('header', { class: styles.header }, 'Header');
+  const svg = SvgIcon({
+    src: wgLogo,
+    alt: 'WG Logo',
+  });
+
+  const logoLink = h('a', { href: 'https://wargaming.net/en', class: styles.logoLink }, svg);
+  const nav = h('nav', {});
+
+  return h('header', { class: styles.header }, [logoLink, nav]);
 }
